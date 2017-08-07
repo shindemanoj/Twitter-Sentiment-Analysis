ab792a5ab13b73fcc72d30f54d1c52a905d5565d
Manoj Shinde <shinde.m@husky.neu.edu>
2017-08-06 23:08:56 -0400

    Adding Twitter Stream and Sentiment Code

---
+++ tweet_sentiment.py
@@ -0,0 +1,35 @@
+import sys
+import json
+
+
+def hw():
+    print('Hello, world!')
+
+
+def lines(fp):
+    print(str(len(fp.readlines())))
+
+
+def main():
+    # sent_file = open(sys.argv[1])
+    # tweet_file = open(sys.argv[2])
+    # hw()
+    # lines(sent_file)
+    # lines(tweet_file)
+    afinnfile = open("AFINN-111.txt")
+    scores = {}  # initialize an empty dictionary
+    for line in afinnfile:
+        term, score = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  # Convert the score to an integer.
+
+    print(scores)  # Print every (term, score) pair in the dictionary
+
+    tweetFile = open("output.txt")
+    tweets = {}  # initialize an empty dictionary
+    for line in tweetFile:
+        tweets.update(json.loads(line))
+    print(tweets)  # Print every (term, score) pair in the dictionary
+
+
+if __name__ == '__main__':
+    main()
