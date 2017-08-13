68d1d92c820b414a3466367c1fb1909a43a00bbf
shindemanoj <shinde.m@husky.neu.edu>
2017-08-13 12:25:07 -0400

    Added Output files and updated code to calculate sentiment score

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,34 +1,24 @@
-import sys
-import json
-
-
-def hw():
-    print('Hello, world!')
-
-
-def lines(fp):
-    print(str(len(fp.readlines())))
-
-
 def main():
-    # sent_file = open(sys.argv[1])
-    # tweet_file = open(sys.argv[2])
-    # hw()
-    # lines(sent_file)
-    # lines(tweet_file)
+    # Open sample sentiment words score file
     afinnfile = open("AFINN-111.txt")
     scores = {}  # initialize an empty dictionary
-    for line in afinnfile:
-        term, score = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+    for tweet in afinnfile:
+        term, score = tweet.split("\t")  # The file is tab-delimited. "\t" means "tab character"
         scores[term] = int(score)  # Convert the score to an integer.
 
     print(scores)  # Print every (term, score) pair in the dictionary
 
+    # Open Tweets file
     tweetFile = open("output.txt")
-    tweets = {}  # initialize an empty dictionary
-    for line in tweetFile:
-        tweets = json.loads(line)
-        print(tweets)
+    for tweet in tweetFile:
+        tweet_words = tweet.split()
+        sentiment_score = 0
+        # Find Sentiment Score for each tweet
+        for word in tweet_words:
+            if word in scores:
+                sentiment_score += scores[word]
+        print(tweet + " : " + str(sentiment_score))
+
 
 if __name__ == '__main__':
     main()
--- twitterstream.py
+++ twitterstream.py
@@ -1,70 +1,28 @@
-import oauth2 as oauth
-import urllib.request as urllib
 import json
 import re
 import tweepy
 from tweepy import OAuthHandler
 from textblob import TextBlob
 
+# Load config file
 with open('config.json', 'r') as f:
     config = json.load(f)
 
+# Set configuration parameters from config file
 api_key = config['consumer_key']
 api_secret = config['consumer_secret']
 access_token_key = config['access_token']
 access_token_secret = config['access_token_secret']
 
-_debug = 0
 
-oauth_token = oauth.Token(key=access_token_key, secret=access_token_secret)
-oauth_consumer = oauth.Consumer(key=api_key, secret=api_secret)
-
-signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
-
-http_method = "GET"
-
-http_handler = urllib.HTTPHandler(debuglevel=_debug)
-https_handler = urllib.HTTPSHandler(debuglevel=_debug)
-
-'''
-Construct, sign, and open a twitter request
-using the credentials above.
-'''
-
-
+# Function to clean tweet text
 def clean_tweet(tweet):
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) | (\w +:\ / \ / \S +)", " ", tweet).split())
 
 
-def twitterreq(url, method, parameters):
-    req = oauth.Request.from_consumer_and_token(oauth_consumer,
-                                                token=oauth_token,
-                                                http_method=http_method,
-                                                http_url=url,
-                                                parameters=parameters)
-
-    req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
-
-    headers = req.to_header()
-
-    if http_method == "POST":
-        encoded_post_data = req.to_postdata()
-    else:
-        encoded_post_data = None
-        url = req.to_url()
-
-    opener = urllib.OpenerDirector()
-    opener.add_handler(http_handler)
-    opener.add_handler(https_handler)
-
-    response = opener.open(url, encoded_post_data)
-
-    return response
-
-
+# override tweepy.StreamListener to add logic to on_status
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
-        # print(status.text)
         # create TextBlob object of passed tweet text
         print(clean_tweet(status.text).encode('utf-8'))
         analysis = TextBlob(clean_tweet(status.text))
@@ -76,11 +34,12 @@ class MyStreamListener(tweepy.StreamListener):
         # else:
         #     print('negative')
 
+    def on_error(self, status_code):
+        print(status_code)
 
+
+# Function to get tweets with the help of Tweepy API
 def fetchsamples():
-    # attempt authentication
-    # try:
-    # create OAuthHandler object
     auth = OAuthHandler(api_key, api_secret)
     # set access token and secret
     auth.set_access_token(access_token_key, access_token_secret)
